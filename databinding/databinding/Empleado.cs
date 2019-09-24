91d679bddcf6d063b4b5f8f04366c075651a8cb9
Alumno <Alumno@PC16>
2019-09-24 09:36:33 -0500

    Empleado

---
+++ databinding/databinding/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databinding
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+
+        public static Empleado GetEmpleado()
+        {
+            var emp = new Empleado()
+            {
+                Nombre = "Diego Sandoval", Titulo = "Todologo"
+            };
+            return emp;
+        }
+    
+    } 
+}
